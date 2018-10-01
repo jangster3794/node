31c2d947ebfe795ee9f1b4ee423b7fa9f42c67ef
Rohan Bunny Jangotra <rohanjangotra03794@gmail.com>
2018-10-01 13:42:26 +0530

    constructor in finish

--- nodejs/finish/flight/index.js
+++ nodejs/finish/flight/index.js
@@ -1,5 +1,5 @@
-module.exports = function(info){
-    var values = {
+var Flight = function(){
+    this.data = {
         number: null,
         origin: null,
         destination: null,
@@ -8,23 +8,27 @@ module.exports = function(info){
         actualDepart:null,
         actualArrive: null
     }
-
-    for ( var prop in values){
-        if(values[prop]!=='undefined'){
-            values[prop] = info[prop]
+    this.fill = function(info){
+        for ( var prop in this.data){
+            if(this.data[prop]!=='undefined'){
+                this.data[prop] = info[prop]
+            }
         }
     }
 
-    var functions = {
-        triggerDepart: function(){
-            values.actualDepart = Date.now()
-        },
-        triggerArrive: function (){
-            values.actualArrive = Date.now()
-        },
-        getInformation: function(){
-            return values
-        }
+    this.triggerDepart =  function(){
+        this.data.actualDepart = Date.now()
     }
-    return functions
+    this.triggerArrive =  function (){
+        this.data.actualArrive = Date.now()
+    }
+    this.getInformation = function(){
+        return this.data
+    }
+}
+
+module.exports = function(info){
+    var instance = new Flight()
+    instance.fill(info)
+    return instance
 }
