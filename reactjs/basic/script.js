bfd75eea8f6b6b28ec68c2f61e11a4f6cd61b6d7
Rohan Bunny Jangotra <rohanjangotra03794@gmail.com>
2018-10-03 14:53:03 +0530

    intro jsx

--- reactjs/basic/script.js
+++ reactjs/basic/script.js
@@ -1,7 +1,12 @@
 const rootElement = document.getElementById('root')
-const element = React.createElement('h3',{
+// const element = React.createElement('h3',{
+//     className : 'container',
+//     children  : ['Hello World.', 'Goodbye world.']
+// })
+const props = {
     className : 'container',
-    children  : ['Hello World.', 'Goodbye world.']
-})
+    children  : 'Hello World'
+}
+const element = <div{...props} />
 
 ReactDOM.render(element,rootElement)
